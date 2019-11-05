2587fca3f12531fdee3b741bd8492647174830c6
REN Hao <renhaojules@163.com>
2019-11-05 22:27:22 +0800

    相对路径替换成了url_for

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 # 作者：hao.ren3
 # 时间：2019/11/5 14:24
 # IDE：PyCharm
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from setting import user, title, posts
 from app.forms import LoginForm
@@ -14,13 +14,14 @@ def index():
                            home=TranslatePage.Home,
                            login=TranslatePage.Login)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('login'))
     return render_template('login.html',
                            title=title,
                            form=form,
