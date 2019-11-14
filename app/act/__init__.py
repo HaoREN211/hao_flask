b9957a57833f49a516c35eb981cd19f8a9a86e76
REN Hao <renhaojules@163.com>
2019-11-14 23:25:17 +0800

    delete loading icon in each bp

--- app/act/__init__.py
+++ app/act/__init__.py
@@ -3,14 +3,7 @@
 # IDE：PyCharm
 
 from flask import Blueprint
-from flask import send_from_directory
-from os.path import join
 
 bp = Blueprint('act', __name__)
-
-def favicon():
-    return send_from_directory(join(bp.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-bp.add_url_rule('/favicon.ico',view_func=favicon)
 
 from app.act.routes import like
--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -3,14 +3,7 @@
 # IDE：PyCharm
 
 from flask import Blueprint
-from flask import send_from_directory
-from os.path import join
 
 bp = Blueprint('auth', __name__)
-
-def favicon():
-    return send_from_directory(join(bp.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-bp.add_url_rule('/favicon.ico',view_func=favicon)
 
 from app.auth.routes import login, logout, register
