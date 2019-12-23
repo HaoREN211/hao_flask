9616b1c265002037d4bc44cba129932673f92595
REN Hao <renhaojules@163.com>
2019-12-23 10:00:16 +0800

    change options of tableau

--- app/data/routes/vote.py
+++ app/data/routes/vote.py
@@ -6,6 +6,7 @@ from app.data import bp
 from json import dumps
 from app.models.Vote import VoteTopic
 import datetime
+from math import floor
 
 @bp.route('/vote/<id>', methods=['POST', 'GET'])
 def vote(id):
@@ -15,10 +16,27 @@ def vote(id):
     list_date, list_data = list([]), list([])
     nb_date = (max_date-min_date).days + 1
 
+    max_cnt = 0
+    min_cnt = 9999
+    interval = 5
+
     for current_index in range(nb_date):
         current_date = (min_date+datetime.timedelta(days=current_index)).strftime("%Y-%m-%d")
-        list_data.append(str(current_topic.get_nb_vote_for_a_day(current_date)))
-        list_date.append('"'+current_date+'"')
+        nb_vote = current_topic.get_nb_vote_for_a_day(current_date)
 
-    json_object = '{vote_cnt: ['+",".join(list_data)+'], vote_date:['+",".join(list_date)+']}'
+        if nb_vote > max_cnt:
+            max_cnt = nb_vote
+        if nb_vote < min_cnt:
+            min_cnt = nb_vote
+
+        # 计算y轴刻度间距
+        if (max_cnt-min_cnt) < 5:
+            interval = 1
+        else:
+            interval = float(float(max_cnt-min_cnt)/float(5))
+
+        list_data.append(str(nb_vote))
+        list_date.append('"'+current_date+'"')
+    json_object = '{vote_cnt: ['+",".join(list_data)+'], vote_date:['+",".join(list_date)+'], max_cnt:'+str(max_cnt)+','\
+                  +' min_cnt:'+str(min_cnt)+', interval:'+str(interval)+'}'
     return dumps(json_object)
